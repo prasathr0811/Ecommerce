7933e65b42918d43b4b14bc315594e3ac21826ae
prasathr0811 <153837559+prasathr0811@users.noreply.github.com>
2025-07-01 22:41:33 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
