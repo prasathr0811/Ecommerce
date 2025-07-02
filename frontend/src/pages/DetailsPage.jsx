fb6a6bd23c24a41098b6e4be490371112397410d
prasathr0811 <153837559+prasathr0811@users.noreply.github.com>
2025-07-02 09:03:30 +0530

    Update DetailsPage.jsx

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -36,7 +36,7 @@ function DetailsPage() {
     }
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}`, {
+      const response = await fetch("/api/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
