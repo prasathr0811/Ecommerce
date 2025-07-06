140243e0e44516424bd06e342244090f231f9c86
prasathr0811 <153837559+prasathr0811@users.noreply.github.com>
2025-07-06 18:56:36 +0530

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,19 +1,46 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  name: String,               // Customer full name
-  email: String,
-  mobile: String,
-  age: String,
-  address: String,
-  productName: String,
-  price: Number,
+  name: {
+    type: String,
+    required: true, // Customer full name
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
   quantity: {
     type: Number,
     default: 1,
+    min: 1,
   },
   productImage: {
     type: String,
+    required: true,
     default: "",
   },
   createdAt: {
